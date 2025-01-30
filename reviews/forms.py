b815433bae5ad2e826a7323a9b04962b71a04501
rahulp <rahulp@qburst.com>
2025-01-30 06:54:46 +0530

    Using Model Form with selected fields.

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,13 +1,7 @@
 from django import forms
+from .models import ReviewModel
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(
-        label="Name", 
-        max_length=10,
-        error_messages={
-            'required': 'Your name must not be empty',
-            'max_length': 'Max length should be 10'
-        }
-    )
-    review = forms.CharField(label="Your Review", max_length=200, widget=forms.Textarea)
-    rating = forms.IntegerField(min_value=1, max_value=5, label="Rating")
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = ReviewModel
+        fields = ['name', 'rating']
