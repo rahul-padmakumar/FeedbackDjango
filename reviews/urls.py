bb1faac4224d7720ba1a0083251685df6e5072b8
rahulp <rahulp@qburst.com>
2025-01-30 07:33:11 +0530

    Implement Class based views

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.review),
-    path("success", views.success)
+    path("", views.ReviewView.as_view()),
+    path("success", views.SuccessView.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,20 +2,24 @@ from django.shortcuts import render
 from .forms import ReviewForm
 from .models import ReviewModel
 from django.http import HttpResponseRedirect
+from django.views import View
 
 # Create your views here.
-def review(req):
-    if req.method == "POST":
+
+class ReviewView(View):
+    def get(self, req):
+        form = ReviewForm()
+        return render(req, "reviews/review.html", {"form":form})
+    
+    def post(self, req):
         form = ReviewForm(req.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/success")
         else:
-            print(form.errors)  
-    else:    
-        form = ReviewForm()
-        print("I am here")
-    return render(req, "reviews/review.html", {"form": form})
+            return render(req, "reviews/review.html", {"form":form})
+        
 
-def success(req):
-    return render(req, "reviews/success.html")
+class SuccessView(View):
+    def get(self, req):
+        return render(req, "reviews/success.html")
