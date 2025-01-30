5536340df74ac8596233d5b0892d9bb45d377e5c
rahulp <rahulp@qburst.com>
2025-01-30 07:44:11 +0530

    Implement Template View with dynamic content

--- reviews/views.py
+++ reviews/views.py
@@ -23,3 +23,7 @@ class ReviewView(View):
 
 class SuccessView(TemplateView):
     template_name = "reviews/success.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'Thank you'
+        return context
