e48575a6e7c5655a25a637d6824c9ee12b8635fc
rahulp <rahulp5875@gmail.com>
2025-01-31 08:49:33 +0530

    Store files locally

--- profiles/views.py
+++ profiles/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
+def save_file(file):
+    print(file.name)
+    with open(f"temp/{file.name}", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
 # Create your views here.
 class ProfileView(View):
     def get(self, req):
         return render(req, "profiles/profile.html")
     def post(self, req):
-        print(req.FILES['image'])
+        save_file(req.FILES['image'])
         return HttpResponseRedirect("/profiles")
