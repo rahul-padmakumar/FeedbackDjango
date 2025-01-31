04b0289c4b82c556d17b833c8cd9d15ea27c4aa8
rahulp <rahulp5875@gmail.com>
2025-01-31 08:34:26 +0530

    Add a basic file upload with printing file name to console

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+class ProfileView(View):
+    def get(self, req):
+        return render(req, "profiles/profile.html")
+    def post(self, req):
+        print(req.FILES['image'])
+        return HttpResponseRedirect("/profiles")
