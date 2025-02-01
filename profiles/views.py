d39b6ffd4d887012256b7499c62fa242cb45b488
rahulp <rahulp5875@gmail.com>
2025-02-01 07:29:48 +0530

    Use CreateView for file upload

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import ProfileForm
 from .models import ProfileModel
+from django.views.generic.edit import CreateView
 
 def save_file(file):
     print(file.name)
@@ -11,26 +12,8 @@ def save_file(file):
             dest.write(chunk)
 
 # Create your views here.
-class ProfileView(View):
-    def get(self, req):
-        form = ProfileForm()
-        return render(
-            req,
-            "profiles/profile.html",
-            {'form': form}
-        )
-    def post(self, req):
-        form = ProfileForm(req.POST, req.FILES)
-        if form.is_valid():
-            model = ProfileModel(
-                image = req.FILES["user_image"]
-            )
-            model.save()
-            return HttpResponseRedirect("/profiles")
-        else: 
-            print(f"FORM NOT{form.errors}")
-            return render(
-                req,
-                "profiles/profile.html",
-                {'form': form}
-            )
+class ProfileView(CreateView):
+    template_name = "profiles/profile.html"
+    model = ProfileModel
+    fields = '__all__'
+    success_url = "/profiles"
